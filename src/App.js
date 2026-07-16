c3fcfc0e87b3df7b9da025ce1408ef63b549ea3c
shabbas-foundation <shivanikarhale40@gmail.com>
2026-07-16 13:40:32 +0530

    shabbaskattahero

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import "./App.css";
 import Footer from "./Common/Footer";
 import Header from "./Common/Header";
-<<<<<<< HEAD
-import MomentGallery from "./pages/HomePage/AamcheKshanachitra";
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <MomentGallery/>
-=======
 import { Routes, Route } from "react-router-dom";
 import ShabbasKattaHero from "./pages/ShabbasKattaPage/ShabbasKattaHero";
 function App() {
@@ -23,7 +14,6 @@ function App() {
         <Route path="/shabbas-katta" element={<ShabbasKattaHero />} />
       </Routes>
 
->>>>>>> 4c7f05fc6fdd77b1d1bb85ac24ffb32d0c9e59e2
       <Footer />
     </div>
   );
--- src/pages/ShabbasKattaPage/ShabbasKattaHero.jsx
+++ src/pages/ShabbasKattaPage/ShabbasKattaHero.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./ShabbasKattaHero.css";
-import heroImage from "../ShabbasKattaPage/Images/shabbaskattahero.jpg";
+import heroImage from "../ShabbasKattaPage/Images/shabbaskattahero.png";
+import { Link } from "react-router-dom";
 
 const ShabbasKattaHero = () => {
   return (
     <section className="shabbaskattahero">
       <div className="shabbaskattaheroContainer">
-
         {/* Left */}
         <div className="shabbaskattaheroLeft">
-
           <h1 className="shabbaskattaheroTitle">
             “इथे फक्त अभ्यास नाही,
             <br />
@@ -17,33 +16,34 @@ const ShabbasKattaHero = () => {
           </h1>
 
           <p className="shabbaskattaheroDescription">
-            शाब्बास अकॅडमी घेऊन येत आहे एक खास डिजिटल मंच जिथे
-            विद्यार्थी, शिक्षक आणि पालक एकत्र येतात - ज्ञान,
-            अनुभव आणि प्रेरणा शेअर करण्यासाठी.
+            शाब्बास अकॅडमी घेऊन येत आहे एक खास डिजिटल मंच जिथे विद्यार्थी,
+            शिक्षक आणि पालक एकत्र येतात - ज्ञान, अनुभव आणि प्रेरणा शेअर
+            करण्यासाठी.
           </p>
 
           <h2 className="shabbaskattaheroSubTitle">
             तुम्हीही या यशस्वी प्रवासाचा भाग व्हा
           </h2>
 
-          <button className="shabbaskattaheroButton">
+          <a
+            href="https://shabbas.in/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="shabbaskattaheroButton"
+          >
             परीक्षा नोंदणी करा
-          </button>
-
+          </a>
         </div>
 
         {/* Right */}
 
         <div className="shabbaskattaheroRight">
-
           <img
             src={heroImage}
             alt="Shabbas Katta"
             className="shabbaskattaheroImage"
           />
-
         </div>
-
       </div>
     </section>
   );
