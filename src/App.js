4c7f05fc6fdd77b1d1bb85ac24ffb32d0c9e59e2
shabbas-foundation <shivanikarhale40@gmail.com>
2026-07-16 12:19:28 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import Footer from "./Common/Footer";
 import Header from "./Common/Header";
-
+import { Routes, Route } from "react-router-dom";
+import ShabbasKattaHero from "./pages/ShabbasKattaPage/ShabbasKattaHero";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+
+        <Route path="/shabbas-katta" element={<ShabbasKattaHero />} />
+      </Routes>
+
       <Footer />
     </div>
   );
 }
-
 export default App;
